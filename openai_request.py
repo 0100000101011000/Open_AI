aa61a8940f3332836f24e77256ce0ca83ae66507
0100000101011000 <alex92_guckheim@web.de>
2023-01-30 10:39:40 +0100

    changes

--- openai_request.py
+++ openai_request.py
@@ -3,7 +3,7 @@ import openai
 import json
 import requests
 
-mykey = "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"
+mykey = "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"
 
 openai.organization = "org-I9o2AkEBNABq4qZXcuBuhS8L"
 openai.api_key = mykey
@@ -18,9 +18,9 @@ headers = {
 
 data = {
     "model": "text-davinci-003",
-    "prompt": "Say this is a test",
+    "prompt": "Das höchste Gebäude in Deutschland",
     "temperature": 0,
-    "max_tokens": 7
+    "max_tokens": 500
 }
 
 response = requests.post(url, headers=headers, json=data)
