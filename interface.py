05e8cb73a822fed6a527b12717639db39e1cf06b
0100000101011000 <alex92_guckheim@web.de>
2023-01-30 20:56:00 +0100

    somechanges

--- interface.py
+++ interface.py
@@ -2,18 +2,25 @@ import tkinter as tk
 
 
 root = tk.Tk()
-root.title("Textbox GUI")
+root.title("AskGPT")
+root.minsize(600,400)
+root.configure(background="#77f296")
 
-textbox1 = tk.Entry(root)
-textbox1.pack()
-
-textbox2 = tk.Entry(root)
-textbox2.pack()
-
-button = tk.Button(root, text="Show Text")
-button.pack()
-
-label = tk.Label(root, text="")
+label = tk.Label(root, text="Frag etwas",font=("Arial",12),background="#77f296",foreground="#FFFFFF")
 label.pack()
+
+askbox = tk.Text(root, width=30, height=6)
+askbox.pack(pady=5)
+
+button = tk.Button(root, text="Tell me!",font=("Arial",12),padx=15)
+button.pack(pady=10)
+
+answerebox = tk.Text(root, width=30, height=6)
+answerebox.configure(state="disabled")
+answerebox.pack()
+
+
+
+
 
 root.mainloop()
