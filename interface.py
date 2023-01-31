e630fe984e382babce791fd06d258b9434728262
0100000101011000 <alex92_guckheim@web.de>
2023-01-31 22:02:23 +0100

    GetInput&PutOutput

--- interface.py
+++ interface.py
@@ -1,14 +1,13 @@
 import tkinter as tk
 from GradientFrame import GradientFrame
 
-def testfunc():
-    print("TestFunktion")
+
+    
 
 class GptWindow(tk.Tk):
 
-    def __init__(self, color1="#FFFFFF", color2="#000000",aFunction=0):
+    def __init__(self, color1="#FFFFFF", color2="#000000"):
 
-        self.aFunction = aFunction
         tk.Tk.__init__(self)
         self.title("Test")
 
@@ -19,22 +18,23 @@ class GptWindow(tk.Tk):
         label = tk.Label(gf, text="Frag etwas:",font=("Arial",12),background="#FFFFFF",foreground="#000000")
         label.pack(pady=20)
 
-        askbox = tk.Text(gf, width=50, height=6)
-        askbox.configure(font=12)
-        askbox.pack(pady=5)
+        self.askbox = tk.Text(gf, width=50, height=6)
+        self.askbox.configure(font=12)
+        self.askbox.pack(pady=5)
 
         button = tk.Button(gf, text="Tell me!",font=("Arial",12),padx=15, command=self.button_command)
         button.pack(pady=20,padx=50)
 
-        answerebox = tk.Text(gf, width=50, height=6)
-        answerebox.configure(state="disabled",font=12)
-        answerebox.pack(pady=50,padx=50)
+        self.answerebox = tk.Text(gf, width=50, height=6)
+        self.answerebox.configure(state="disabled",font=12)
+        self.answerebox.pack(pady=50,padx=50)
 
     def button_command(self):
-        if self.aFunction != 0:
-            self.aFunction()
-        else:
-            print("Funktion fehlt")
+        question = self.askbox.get("0.0", "end")
+        self.answerebox.configure(state="normal")
+        self.answerebox.delete("0.0", "end")
+        self.answerebox.insert("0.0", question)
+        self.answerebox.configure(state="disabled")
 
-root = GptWindow(color2="#34eb74",aFunction=testfunc)
+root = GptWindow(color2="#34eb74")
 root.mainloop()
