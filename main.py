05bbee34b8c7a3a35384940eb0afc0c871931515
Alexander Schumacher <106386405+AlexNinetytwo@users.noreply.github.com>
2023-02-14 21:55:52 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from filemanager import searchKey
 key = searchKey()
 
 
-# Creats an instanse of the main window
+# Creats an instance of the main window
 root = GptWindow(color1="#7657e3",
                  color2="#ffa247",
                  key=key,
